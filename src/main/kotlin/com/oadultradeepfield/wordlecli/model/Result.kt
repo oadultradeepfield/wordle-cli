48452477fe7646d1d57c7139ad4da9a059fa3ed1
phanuphat <phanuphat@bytedance.com>
2026-01-22 11:32:28 +0800

    :art: Remove unused map function in Result model

--- src/main/kotlin/com/oadultradeepfield/wordlecli/model/Result.kt
+++ src/main/kotlin/com/oadultradeepfield/wordlecli/model/Result.kt
@@ -9,17 +9,4 @@ package com.oadultradeepfield.wordlecli.model
 sealed class Result<out T> {
     data class Ok<T>(val value: T) : Result<T>()
     data class Err(val message: String) : Result<Nothing>()
-
-    /**
-     * Non-Inline function: During runtime, there will be an anonymous class created for the lambda parameter.
-     * Inline function: The compiler replaces the function call with actual function body. There will be less runtime
-     * and memory overhead.
-     *
-     * Further Note: Inline function is usually used with 'reified' which is a keyword use with generic types to make it
-     * survives the type-erasure and accessible at runtime.
-     */
-    inline fun <R> map(transform: (T) -> R): Result<R> = when (this) {
-        is Ok -> Ok(transform(value))
-        is Err -> Err(message)
-    }
 }
